4b7f234ab61ff59adfe77a15e8dd3ad00b119c6b
Jose <josebrouwer1@gmail.com>
2024-05-31 10:49:08 -0400

    initial commit: Model, starter HTML

---
+++ Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExcelParsing.Models
+{
+    public class Person
+    {
+        [Key] //Primary Key
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "First Name cannot be longer than 50 characters.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Last Name cannot be longer than 50 characters.")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Age must be a positive number.")]
+        public int Age { get; set; }
+
+        [Required]
+        [RegularExpression("Active|Inactive|Hold", ErrorMessage = "Status must be Active, Inactive, or Hold.")]
+        public string Status { get; set; }
+    }
+}
